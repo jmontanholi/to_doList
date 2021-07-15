a0aca8a6eb108444879d42cb4e6eb80c954dc945
jmontanholi <jonunes9326@hotmail.com.br>
2021-07-15 14:11:26 -0300

    Finish clear all btn functionalities

--- src/_checkboxFunctions.js
+++ src/_checkboxFunctions.js
@@ -1,4 +1,6 @@
 const checkboxes = document.getElementsByClassName('checkbox');
+const clearAll = document.getElementById('clearAll');
+const div = document.getElementById('itemsDiv');
 // eslint-disable-next-line import/no-mutable-exports
 export let list = [];
 
@@ -26,3 +28,45 @@ export default function checkboxesEvent(list) {
     });
   }
 }
+
+clearAll.addEventListener('click', () => {
+  const filtered = list.filter((element) => {
+    if (element.completed === false) {
+      return true;
+    }
+    return false;
+  });
+  list = filtered;
+  if (window.confirm('Are you sure?')) {
+    if (list.length === 0) {
+      div.innerHTML = '';
+    } else {
+      div.innerHTML = '';
+      list.forEach((element) => {
+        const li = document.createElement('li');
+        li.draggable = true;
+        li.classList.add('dropzone');
+        if (element.completed === true) {
+          li.classList.add('completed');
+        }
+        if (element.completed) {
+          li.innerHTML = `
+            <input class="checkbox dropzone2" type="checkbox" id="${element.index}" checked>
+            <p class="description dropzone2">${element.description}</p>
+            <p class="dots dropzone2"><i class="bi bi-three-dots-vertical"></i></p>
+          `;
+        } else {
+          li.innerHTML = `
+            <input class="checkbox dropzone2" type="checkbox" id="${element.index}">
+            <p class="description dropzone2">${element.description}</p>
+            <p class="dots dropzone2"><i class="bi bi-three-dots-vertical"></i></p>
+          `;
+        }
+        li.classList.add('d-flex', 'justify-content-around', 'align-content-center');
+        div.appendChild(li);
+      });
+    }
+  }
+  checkboxesEvent(list);
+  saveLocalstorage();
+});
