06f9f58746921e00486f8debdaf616f85fa80143
jmontanholi <jonunes9326@hotmail.com.br>
2021-07-15 12:13:07 -0300

    Finish add with addBtn

--- src/_addRemoveFunctions.js
+++ src/_addRemoveFunctions.js
@@ -2,6 +2,7 @@ import { list } from './_checkboxFunctions.js';
 import { populate } from './_dragFunctions.js';
 
 const textInput = document.getElementById('textInput');
+const addBtn = document.getElementById('addBtn');
 
 class Task {
   constructor(description, completed = false, index = (list.length + 1)) {
@@ -31,5 +32,22 @@ textInput.addEventListener('keypress', (event) => {
       populate();
       textInput.value = '';
     }
+  }
+});
+
+addBtn.addEventListener('click', () => {
+  if (textInput.value.trim() === '') {
+    textInput.placeholder = 'Please insert a valid value';
+    textInput.classList.add('border', 'border-danger');
+    textInput.addEventListener('change', () => {
+      if (textInput.value.trim() !== '') {
+        textInput.classList.remove('border', 'border-danger');
+      }
+    });
+  } else {
+    const task = new Task(textInput.value);
+    task.addTask();
+    populate();
+    textInput.value = '';
   }
 });
